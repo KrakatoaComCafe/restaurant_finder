35f98b2417af8106e9d300cbec534ad5d96a6ee7
Tiago Vidotto <tiagovidotto@gamil.com>
2025-06-06 15:27:00 -0300

    initial commit with spring initializr

---
+++ src/main/kotlin/com/krakatoa/restaurantfinder/RestaurantfinderApplication.kt
@@ -0,0 +1,11 @@
+package com.krakatoa.restaurantfinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestaurantfinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestaurantfinderApplication>(*args)
+}
